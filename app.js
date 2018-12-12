85be1ac21dcc517219fa5bd15978c2deb4dc2628
Joshua Milcette <joshuamilcette.student@careerdevs.com>
2018-12-12 01:44:11 +0000

    titles, Array.isArray, .forEach

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ console.log(Array.isArray(Array.from(titles)));
 Array.from(titles).forEach(function(title){
   console.log(title);
 });
+console.log("Testing");
+titles.forEach(function(item){
+    console.log("item:"+item);
+});
