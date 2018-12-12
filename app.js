6ffa4efe4426353262d7e07dbecf96e2506bfb68
Joshua Milcette <joshuamilcette.student@careerdevs.com>
2018-12-12 01:56:27 +0000

    const wrap = document.querySelector('#ook-list li:nth-child(2) .name')

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const wrap = document.querySelector('#wrapper');
+const wrap = document.querySelector('#ook-list li:nth-child(2) .name');
 console.log(wrap);
 
 const wrap2 = document.getElementById("wrapper").innerHTML;
