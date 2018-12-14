7ad0e7a3aa0c1f0da0edac8f33abd6f78c4b46ca
Joshua Milcette <joshuamilcette.student@careerdevs.com>
2018-12-14 00:56:59 +0000

    added comments

--- app.js
+++ app.js
@@ -6,3 +6,9 @@ var books = document.querySelector('#book-list list .name')
 //Using this method, you have to use .forEach() to turn it into an array
 var books2 = document.getElementsByClassName('name');
 console.log(books2);
+
+Array.from(books).forEach(function(book){
+    
+//Using .textContent to change the text inside the selected querys in my books variable
+    book.textContent += '(book title)';
+})
