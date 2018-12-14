8068301644d38395cae6b5967d6a8cfc07e5c00e
Joshua Milcette <joshuamilcette.student@careerdevs.com>
2018-12-14 00:45:54 +0000

    using DOM to change text with .textContent

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-const wrap = document.querySelector('#Wrapper')
+const wrap = document.querySelector('#Wrapper');
+
+//Using this method, you do not need to turn it into an array
+var books = document.querySelector('#book-list list .name')
+
+//Using this method, you have to use .forEach() to turn it into an array
+var books2 = document.getElementsByClassName('name');
+console.log(books2);
