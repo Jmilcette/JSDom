8f59d62fba7b3c8bc8b6a6ab6c878aeca7df4c5f
Joshua Milcette <joshuamilcette.student@careerdevs.com>
2018-12-14 01:28:24 +0000

    using .hasChildNodes to check if the selected node has child nodes

--- app.js
+++ app.js
@@ -5,3 +5,6 @@ console.log('#page-banner node type is:', banner.nodeType);
 
 //This line will display the node name which would be DIV in this instance
 console.log('#page-banner node name is:', banner.nodeName);
+
+//This line will give a true or false statement on whether or not there are child nodes on the selected element
+console.log('#page-banner node name is:', banner.hasChildNodes());
