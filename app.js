02872efa69836486a47aa1b63c4f1de60e0e0eba
Joshua Milcette <joshuamilcette.student@careerdevs.com>
2018-12-14 01:08:44 +0000

    minor change

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ console.log(books2);
 
 Array.from(books).forEach(function(book){
     
-//Using .textContent to change the text inside the selected querys in my books variable
+//Using .textContent to change the text inside the selected query in my books variable
     book.textContent += '(book title)';
 })
