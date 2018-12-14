56432fadbbd372709d77612fb1fc4cf809962f95
Joshua Milcette <joshuamilcette.student@careerdevs.com>
2018-12-14 01:26:04 +0000

    using .nodeName to find the name of the selected node

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 //In this line were taking the banner id with the query selector
 const banner = document.querySelector('#page-banner');
 //This line will display the node type as 1 which is element
-console.log('#page-banner node type is:', banner.nodeType)
+console.log('#page-banner node type is:', banner.nodeType);
+
+//This line will display the node name which would be DIV in this instance
+console.log('#page-banner node name is:', banner.nodeName);
