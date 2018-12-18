c096d7e5ce213ef66b4c6053f890692e7de4e3b6
Joshua Milcette <joshuamilcette.student@careerdevs.com>
2018-12-18 22:14:09 +0000

    using.parentNode

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-//In this line were taking the banner id with the query selector
-const banner = document.querySelector('#page-banner');
-//This line will display the node type as 1 which is element
-console.log('#page-banner node type is:', banner.nodeType);
+const bookList = document.querySelector('#book-list');
 
-//This line will display the node name which would be DIV in this instance
-console.log('#page-banner node name is:', banner.nodeName);
-
-//This line will give a true or false statement on whether or not there are child nodes on the selected element
-console.log('#page-banner has child nodes:', banner.hasChildNodes());
-
-//You can clone a node with .cloneNode and passing it as true to go deeper into the DOM tree
-const clonedBanner =banner.cloneNode(true);
-console.log(clonedBanner);
+console.log('the parent node is:', bookList.parentNode);
