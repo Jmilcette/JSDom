3b18211cb543a848ed9cfcf40687f126987edf8c
Joshua Milcette <joshuamilcette.student@careerdevs.com>
2018-12-18 22:56:00 +0000

    intro to .parentNode and .parentElement

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 const bookList = document.querySelector('#book-list');
-
+//using .parentNode grabs the parent or the node that the selected node is under
+console.log('the parent node is:', bookList.parentNode);
+//using .parentElement basically does the same thing as .parentNode
 console.log('the parent node is:', bookList.parentNode);
