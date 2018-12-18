2325fb6f2ef22ea99f240afe8a5824d0316c128f
Joshua Milcette <joshuamilcette.student@careerdevs.com>
2018-12-18 23:00:27 +0000

    Traversing sibling elements with .nextSibling

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
+//TRAVERSING SIBLING ELEMENTS
+
 const bookList = document.querySelector('#book-list');
-//using .parentNode grabs the parent or the node that the selected node is under
-console.log('the parent node is:', bookList.parentNode);
-//using .parentElement basically does the same thing as .parentNode
-console.log('the parent node is:', bookList.parentNode);
+
+console.log('book-list next siling is:', bookList.nextSibling);
