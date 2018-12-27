b6b5f587b0ec5e512d326c0ed455f81796eb9fb4
Joshua Milcette <joshuamilcette.student@careerdevs.com>
2018-12-27 03:07:29 +0000

    testing commits

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const titles = document.getElementsByClassName('title');
 console.log(Array.isArray(titles));
 console.log(Array.isArray(Array.from(titles)));
 
+
 Array.from(titles).forEach(function(title){
   console.log(title);
 });
